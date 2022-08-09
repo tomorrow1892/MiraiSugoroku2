9028ff74617c8f353c49a3cefb0e48eb9237ca8f
Horie <h.horie916@gmail.com>
2022-08-09 11:26:59 +0900

    マス作成コントローラ

---
+++ miraiSugoroku/src/main/java/ksp/group3/miraiSugoroku/entity/SquareEvent.java
@@ -0,0 +1,24 @@
+package ksp.group3.miraiSugoroku.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class SquareEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long squareEventID;
+    int actionNumber;
+    int argument;
+    String eventTitle;
+
+}
